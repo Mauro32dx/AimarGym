3e953c81d784b6bd0df641bacca98c215e9bb30a
Mauro32dx <mauromam32dx@gmail.com>
2020-10-26 03:49:52 -0300

    comit en MAIN 1

---
+++ build/precache-manifest.d2cc30a56baa9889b5407650b2c4fa1a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcfeb22ea8f802c6b1befab443fb87b",
+    "url": "/aimar/index.html"
+  },
+  {
+    "revision": "85851f9987c40ab61ed8",
+    "url": "/aimar/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4f89ebc54988c5ec6849",
+    "url": "/aimar/static/css/main.0e80b1ad.chunk.css"
+  },
+  {
+    "revision": "85851f9987c40ab61ed8",
+    "url": "/aimar/static/js/2.1bd55205.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/aimar/static/js/2.1bd55205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f89ebc54988c5ec6849",
+    "url": "/aimar/static/js/main.60381fc9.chunk.js"
+  },
+  {
+    "revision": "d8f31a5270ec08b6eb66",
+    "url": "/aimar/static/js/runtime-main.da3bd494.js"
+  },
+  {
+    "revision": "161f493b7d01063e1ac98a4e729aaa72",
+    "url": "/aimar/static/media/classes-item-1.161f493b.jpg"
+  },
+  {
+    "revision": "39756a1323d6c1b4f6c0d3eb1fd8768c",
+    "url": "/aimar/static/media/classes-item-2.39756a13.jpg"
+  },
+  {
+    "revision": "c6de269ed3038b394e608575938410cc",
+    "url": "/aimar/static/media/classes-item-3.c6de269e.jpg"
+  },
+  {
+    "revision": "32f0b9b6ed86af2d2cee7c7a95ae231b",
+    "url": "/aimar/static/media/classes-item-4.32f0b9b6.jpg"
+  },
+  {
+    "revision": "f97741d7065c6593bb482bc6fd2ad946",
+    "url": "/aimar/static/media/classes-item-5.f97741d7.jpg"
+  },
+  {
+    "revision": "29e39b85932156771deaf7607353a461",
+    "url": "/aimar/static/media/classes-item-6.29e39b85.jpg"
+  },
+  {
+    "revision": "705a043210c6592b64f4d467d90f37d6",
+    "url": "/aimar/static/media/fondo1.705a0432.jpg"
+  },
+  {
+    "revision": "2cf2179a6aa7041bdecdb444ba73c072",
+    "url": "/aimar/static/media/perfil1.2cf2179a.png"
+  },
+  {
+    "revision": "d7fea4770c9efefc9650c4a01d1f6b4b",
+    "url": "/aimar/static/media/perfil2.d7fea477.png"
+  },
+  {
+    "revision": "f14860c535764622acf540e0ab25fbcd",
+    "url": "/aimar/static/media/perfil3.f14860c5.png"
+  },
+  {
+    "revision": "2ef01a62f2d84844e6f7ca18505b3039",
+    "url": "/aimar/static/media/perfil4.2ef01a62.png"
+  },
+  {
+    "revision": "ae1dd03a83654d6f6417eef0538f1af5",
+    "url": "/aimar/static/media/perfil5.ae1dd03a.png"
+  },
+  {
+    "revision": "0c30b0986f131ab0b7fea1d72aad4616",
+    "url": "/aimar/static/media/perfil6.0c30b098.png"
+  },
+  {
+    "revision": "62da7fcc332aa5d25b46b8f45a233886",
+    "url": "/aimar/static/media/perfil7.62da7fcc.png"
+  },
+  {
+    "revision": "51892308131c94122bf61584d0eec4c4",
+    "url": "/aimar/static/media/slide-1.51892308.png"
+  },
+  {
+    "revision": "85cd6d08039ed9d8e19e48a188b9d5ae",
+    "url": "/aimar/static/media/slide-2.85cd6d08.png"
+  },
+  {
+    "revision": "bf06b00905c049ecaff96bf6e0ea47ed",
+    "url": "/aimar/static/media/slide-3.bf06b009.png"
+  }
+]);
